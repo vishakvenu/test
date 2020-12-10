00d831a7fd9f644b9c75ac96f6567c17a00d72e3
vishak <vishakvenu91@gmail.com>
2020-12-10 10:31:45 +0530

    changeed script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.to(".img", {duration:1,opacity:0});
+gsap.to(".img1", {duration:1.5,delay:1,opacity:0,scale:0});
+gsap.to(".img2", {duration:0.5,scale:3,duration:2,opacity:0,ypercent:100});
+//gsap.to(".img", {x:700,duration:3,borderRadius:100,rotation:360,ease:"power3"});
